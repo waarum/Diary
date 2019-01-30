9d79b20e6bccd65e7398fc16edee36e20080eef0
Waarum <merawataru@meryouwatarunomacbook-air.local>
2019-01-30 23:52:45 +0900

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by 目良渉 on 2019/01/30.
+//  Copyright © 2019 Wataru Mera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
